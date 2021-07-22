c46d0f54339a1bd7ad64d8f01c5cf3caf66daeb9
erfinon <erfinon@gmail.com>
2021-07-22 23:24:27 +0430

    push salam

--- init_analyze.py
+++ init_analyze.py
@@ -1,13 +1,15 @@
 from analyze import *
-print(os.path.exists("pack/temp/log000.txt"))
+print(os.path.exists("temp/log000.txt"))
 # all available - indexed from 0
-print(len(os.listdir("pack/temp")))
+print(len(os.listdir("temp")))
 #initialize into all.json
 print(os.getcwd())
+print("enter file amount >")
+climit = int(input())
 c = 0
-while(c < 5):#len(os.listdir("temp"))):
-    if(os.path.exists("pack/temp/log{}.txt".format(lformat(c)))): 
-        f = open("pack/temp/log{}.txt".format(lformat(c)))
+while(c < climit):#len(os.listdir("temp"))):
+    if(os.path.exists("temp/log{}.txt".format(lformat(c)))): 
+        f = open("temp/log{}.txt".format(lformat(c)))
         for i in f:
             time_ms = json.loads(i)["time_ms"]
             time = json.loads(i)["time"]
@@ -18,9 +20,9 @@ while(c < 5):#len(os.listdir("temp"))):
                 "message":[message]
             })
             data = data.append(data1,ignore_index=True)
+            print("{}:{}".format(c,len(os.listdir("temp"))))
     c+=1
 
-    print("{}:{}".format(c,len(os.listdir("pack/temp"))))
 data.to_json("all.json")
 data.to_csv("all.csv")
 print(data.head())
